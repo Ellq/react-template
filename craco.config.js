66c63cb3edf7f25bf1cf4e101b0eba8f34332f75
ellq <lastquincy13@gmail.com>
2021-12-10 11:49:44 +0500

    diff fixes

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,7 @@
-const StylelintPlugin = require("stylelint-webpack-plugin")
+const StylelintPlugin = require("stylelint-webpack-plugin");
 
 module.exports = {
   webpack: {
-    plugins: [new StylelintPlugin({ fix: true })]
-  }
-}
+    plugins: [new StylelintPlugin({ fix: true })],
+  },
+};
